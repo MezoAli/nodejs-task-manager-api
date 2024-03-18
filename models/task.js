c7bd259f3c3e22a63977a5d64a13705b47935b08
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-19 01:58:18 +0200

    adding validation to our schema

--- models/task.js
+++ models/task.js
@@ -4,9 +4,13 @@ const TaskSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: [true, "please provide name"],
+      trim: true,
+      maxlength: [20, "name can not be more than 20 characters"],
     },
     completed: {
       type: Boolean,
+      default: false,
     },
   },
   {
