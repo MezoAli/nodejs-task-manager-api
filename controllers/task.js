e9a970d3160bca682bebf5a361336fc4ab2d1db3
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-19 02:43:09 +0200

    updating error returns to distinguish between cast errors and other errors

--- controllers/task.js
+++ controllers/task.js
@@ -12,6 +12,9 @@ const getSingleTask = async (req, res) => {
   try {
     const id = req.params.id;
     const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ msg: `no Task with id : ${id}` });
+    }
     res.status(200).json({ success: true, task });
   } catch (error) {
     res.status(500).json({ msg: error });
@@ -21,6 +24,9 @@ const updateTask = async (req, res) => {
   try {
     const id = req.params.id;
     const updatedTask = await Task.findByIdAndUpdate(id, req.body);
+    if (!updatedTask) {
+      return res.status(404).json({ msg: `no Task with id : ${id}` });
+    }
     res.status(201).json({ success: true, task: updatedTask });
   } catch (error) {
     res.status(500).json({ msg: error });
@@ -29,7 +35,10 @@ const updateTask = async (req, res) => {
 const deleteTask = async (req, res) => {
   try {
     const id = req.params.id;
-    await Task.findByIdAndDelete(id);
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ msg: `no Task with id : ${id}` });
+    }
     res.status(200).json({ success: true, msg: "Deleted Successfully" });
   } catch (error) {
     res.status(500).json({ msg: error });
